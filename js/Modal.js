6605689938834bf5ae9798b78ac8d44e4f0d719d
Ishrat Abbas <115890257+ishtuuabbas@users.noreply.github.com>
2024-02-07 15:32:28 +0500

    Add files via upload

---
+++ js/Modal.js
@@ -0,0 +1,10 @@
+function Modal(){
+    return (
+    <div className="modall">
+        <p>Are you sure?</p>
+        <button className="btn btn--alt">Cancel</button>
+        <button>Confirm</button>
+        </div>
+    );
+}
+export default Modal;
